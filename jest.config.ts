247fd2eb0d54551318fa159c8f7baa18256c3430
taila <tailaspl@gmail.com>
2025-07-19 17:41:04 -0300

    chore: installing libs and config files for developing tests

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json'
+    }
+  },
+};
+
+export default config;
